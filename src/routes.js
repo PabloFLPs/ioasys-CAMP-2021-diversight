46caf259b8060c88e61d0145a2cecfefa4fe3c19
PabloFLPs <pablofelipe1999@hotmail.com>
2021-06-01 21:22:23 -0300

    Dashboards fictional data and Register Dashboard Route added

--- src/routes.js
+++ src/routes.js
@@ -76,7 +76,7 @@ routes.get('/', (req, res) => {
     routeRespostaList: "/respostas (GET) -> lista todas as respostas de formularios",
     routeUserUpdate: "/user_update (POST) -> atualiza dados {nome, raca, genero, orient_sex, pcd, data_nasc, empresa, area_atuac, cargo} do usuario, filtrado pelo id {id}",
     routeRegisterDashboard: "/dashboard_register (POST) -> registra dashboard de empresa {empresa, NPS_CR_Guia, NPS_CR, NPS_G_Guia, NPS_G, NPS_O_Guia, NPS_O, NPS_PCS_Guia, NPS_PCS, NPS_Geral_Guia, NPS_Geral}",
-    routeFilterDashboard: "/dashboard_list (POST) -> filtra e mostra dashboard por empresa {empresa}",
+    routeFilterDashboard: "/dashboard_filter (POST) -> filtra e mostra dashboard por empresa {empresa}",
     routeListDashboard: "/dashboard (GET) -> lista dashboards"
   });
 });
